616bb754e6bf8ba99df6b04df941e2ee26294d5b
Ngô Quốc Đạt <56961917+datlechin@users.noreply.github.com>
2022-08-12 03:50:47 +0700

    feat: Vietnamese translations (#19)

---
+++ resources/lang/vi/filament-authentication.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    'section.group'                 => 'Xác thực',
+    'section.users'                 => 'Người dùng',
+    'section.user'                  => 'Người dùng',
+
+
+    /*
+        |--------------------------------------------------------------------------
+        | Fields
+        |--------------------------------------------------------------------------
+    */
+    'field.id'                      => 'ID',
+    'field.user.name'               => 'Họ tên',
+    'field.user.email'              => 'Email',
+    'field.user.roles'              => 'Vai trò',
+    'field.user.verified_at'        => 'Đã xác thực',
+    'field.user.created_at'         => 'Tham gia lúc',
+    'field.user.password'           => 'Mật khẩu',
+    'field.user.confirm_password'   => 'Mật khẩu xác nhận',
+
+    'field.guard_name'              => 'Tên guard',
+    'field.name'                    => 'Tên',
+    'field.permissions'             => 'Quyền',
+    'field.roles'                   => 'Vai trò',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Labels
+        |--------------------------------------------------------------------------
+    */
+
+    'section.permission'            => 'Quyền',
+    'section.permissions'           => 'Quyền',
+    'section.role'                  => 'Vai trò',
+    'section.roles'                 => 'Vai trò',
+    'section.roles_and_permissions' => 'Vai trò và Quyền',
+
+
+    /*
+        |--------------------------------------------------------------------------
+        | Filters
+        |--------------------------------------------------------------------------
+    */
+    'filter.verified'               => 'Đã xác thực email',
+
+    'button.impersonate'            => "Người dùng mạo danh",
+    'text.impersonating'            => 'Mạo danh người dùng: ',
+    'text.impersonating.end'        => ' - Kết thúc Mạo danh ',
+];
