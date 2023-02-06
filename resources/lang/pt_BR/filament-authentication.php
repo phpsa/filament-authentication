93f838e7c8dcd24d0e0f77f9c8b997addace98ea
Daniel Neto <danielneto314@gmail.com>
2023-02-06 17:48:57 -0300

    feat: Portuguese translations (#30)

---
+++ resources/lang/pt_BR/filament-authentication.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    'section.group'                 => 'Autenticação',
+    'section.users'                 => 'Usuários',
+    'section.user'                  => 'Usuário',
+
+
+    /*
+        |--------------------------------------------------------------------------
+        | Fields
+        |--------------------------------------------------------------------------
+    */
+    'field.id'                      => 'ID',
+    'field.user.name'               => 'Nome',
+    'field.user.email'              => 'E-mail',
+    'field.user.roles'              => 'Função',
+    'field.user.verified_at'        => 'Verificado em',
+    'field.user.created_at'         => 'Criado em',
+    'field.user.password'           => 'Senha',
+    'field.user.confirm_password'   => 'Confimação da Senha',
+
+    'field.guard_name'              => 'Nome do Guarda',
+    'field.name'                    => 'Nome',
+    'field.permissions'             => 'Permissões',
+    'field.roles'                   => 'Funções',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Labels
+        |--------------------------------------------------------------------------
+    */
+
+    'section.permission'            => 'Permissão',
+    'section.permissions'           => 'Permissões',
+    'section.role'                  => 'Função',
+    'section.roles'                 => 'Funções',
+    'section.roles_and_permissions' => 'Funções e Permissões',
+
+
+    /*
+        |--------------------------------------------------------------------------
+        | Filters
+        |--------------------------------------------------------------------------
+    */
+    'filter.verified'               => 'E-mail Verificado',
+
+    'button.impersonate'            => "Personificar Usuário",
+    'text.impersonating'            => 'Personificando o Usuário: ',
+    'text.impersonating.end'        => ' - Encerrar Perssonificação ',
+];
