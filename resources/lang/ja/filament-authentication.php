d8e94a5bb13104fee1c6297478ec1cc36a9201ad
SHIBAHARA Hiroki <shibomb@gmail.com>
2023-07-25 07:17:45 +0900

    fix(translation): Japanese Translation (#40)

---
+++ resources/lang/ja/filament-authentication.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    'section.group'                 => '認証',
+    'section.users'                 => 'ユーザー',
+    'section.user'                  => 'ユーザー',
+
+
+    /*
+        |--------------------------------------------------------------------------
+        | Fields
+        |--------------------------------------------------------------------------
+    */
+    'field.id'                      => 'ID',
+    'field.user.name'               => '名前',
+    'field.user.email'              => 'メールアドレス',
+    'field.user.roles'              => 'ロール',
+    'field.user.verified_at'        => '認証済み',
+    'field.user.created_at'         => '参加',
+    'field.user.password'           => 'パスワード',
+    'field.user.confirm_password'   => 'パスワードの確認',
+
+    'field.guard_name'              => 'ガード名',
+    'field.name'                    => '名称',
+    'field.permissions'             => '権限',
+    'field.roles'                   => 'ロール',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Labels
+        |--------------------------------------------------------------------------
+    */
+
+    'section.permission'            => '権限',
+    'section.permissions'           => '権限',
+    'section.role'                  => 'ロール',
+    'section.roles'                 => 'ロール',
+    'section.roles_and_permissions' => 'ロールと権限',
+
+
+    /*
+        |--------------------------------------------------------------------------
+        | Filters
+        |--------------------------------------------------------------------------
+    */
+    'filter.verified'               => '認証済みEメール',
+
+    'button.impersonate'            => "なりすましユーザー",
+    'text.impersonating'            => 'なりすましユーザー: ',
+    'text.impersonating.end'        => ' - なりすまし終了 ',
+];
