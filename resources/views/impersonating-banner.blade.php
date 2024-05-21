fc2ceef784e1da276ed450edaa58765c6b9407f7
Craig Smith <952595+phpsa@users.noreply.github.com>
2024-05-21 20:34:54 +1200

    fix: do not register routes / middlware for disabled functionality

--- resources/views/impersonating-banner.blade.php
+++ resources/views/impersonating-banner.blade.php
@@ -1,1 +1,22 @@
-<x-filament-authentication::banner :fixed="true" />
+<style>
+html, body>div>aside {
+    margin-top: 40px;
+}
+body>div>aside
+@media print {
+    html {
+        margin-top: 0;
+    }
+}
+</style>
+
+<div
+    id="impersonating-banner"
+    class="print:hidden bg-primary-500 absolute h-10 top-0 w-full flex items-center content-center justify-center text-gray-800"
+    >
+    <div>
+        {{ __('filament-authentication::filament-authentication.text.impersonating') }} <strong>{{ $impersonating }}</strong>
+        <a href="{{ route("filament.{$panel}.fa.stop.impersonation") }}"><strong>{{ __('filament-authentication::filament-authentication.text.impersonating.end') }}</strong></a>
+    </div>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,15 @@ use Illuminate\Support\Facades\Route;
 use Phpsa\FilamentAuthentication\Actions\ImpersonateLink;
 use Phpsa\FilamentAuthentication\Pages\Auth\RenewPassword;
 
-Route::get('/impersonate/stop', fn () => ImpersonateLink::leave())
-    ->name('filament-authentication.stop.impersonation')
-    ->middleware('web');
-
-
-
 Route::name('filament.')->group(function () {
     foreach (Filament::getPanels() as $panel) {
+        if ($panel->hasPlugin('filament-authentication') === false) {
+            continue;
+        }
+
+        /** @var \Phpsa\FilamentAuthentication\FilamentAuthentication */
+        $plugin = $panel->getPlugin('filament-authentication');
+
         $domains = $panel->getDomains();
 
         foreach ((empty($domains) ? [null] : $domains) as $domain) {
@@ -20,8 +21,16 @@ Route::name('filament.')->group(function () {
                 ->middleware($panel->getMiddleware())
                 ->name($panel->getId() . '.')
                 ->prefix($panel->getPath())
-                ->group(function () use ($panel) {
-                    Route::get('password/renew', RenewPassword::class)->name('auth.password.renew');
+                ->group(function () use ($panel, $plugin) {
+
+                    if ($plugin->impersonateEnabled() === false) {
+                        Route::get('/impersonate/stop', fn () => ImpersonateLink::leave())
+                        ->name('fa.stop.impersonation');
+                    }
+
+                    if ((int) config('filament-authentication.password_renew.renew_password_days_period', 0) > 0) {
+                        Route::get('password/renew', RenewPassword::class)->name('fa.password.renew');
+                    }
                 });
         }
     }
