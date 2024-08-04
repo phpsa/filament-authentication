ac37198eb00b958dee45859980dd8f6460de1aae
Craig Smith <952595+phpsa@users.noreply.github.com>
2024-08-04 13:22:44 +1200

    fix: tidy user table - make more customisable

--- src/FilamentAuthenticationProvider.php
+++ src/FilamentAuthenticationProvider.php
@@ -3,12 +3,13 @@
 namespace Phpsa\FilamentAuthentication;
 
 use Livewire\Livewire;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Event;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Phpsa\FilamentAuthentication\Commands\InstallCommand;
-use Phpsa\FilamentAuthentication\Commands\UpdateUserPasswordToUpdatedCommand;
 use Phpsa\FilamentAuthentication\Pages\Auth\RenewPassword;
+use Phpsa\FilamentAuthentication\Commands\UpdateUserPasswordToUpdatedCommand;
 use Phpsa\FilamentAuthentication\Subscribers\AuthenticationLoggingSubscriber;
 
 class FilamentAuthenticationProvider extends PackageServiceProvider
@@ -37,5 +38,19 @@ class FilamentAuthenticationProvider extends PackageServiceProvider
         Livewire::component('phpsa.filament-authentication.pages.auth.renew-password', RenewPassword::class);
 
         parent::packageBooted();
+
+        Arr::macro('pushBefore', function (array $existing, $key, $new): array {
+            $keys = array_keys($existing);
+            $index = array_search($key, $keys);
+            $pos = false === $index ? count($existing) : $index - 1;
+            return array_merge(array_slice($existing, 0, $pos), $new, array_slice($existing, $pos));
+        });
+
+        Arr::macro('pushAfter', function (array $existing, $key, $new): array {
+            $keys = array_keys($existing);
+            $index = array_search($key, $keys);
+            $pos = false === $index ? count($existing) : $index + 1;
+            return array_merge(array_slice($existing, 0, $pos), $new, array_slice($existing, $pos));
+        });
     }
 }
--- src/Resources/UserResource.php
+++ src/Resources/UserResource.php
@@ -2,16 +2,15 @@
 
 namespace Phpsa\FilamentAuthentication\Resources;
 
+use Filament\Forms\Get;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
-use Illuminate\Support\Arr;
 use Filament\Resources\Resource;
 use Illuminate\Support\Facades\Hash;
 use Filament\Forms\Components\Select;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\TagsColumn;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Model;
 use Filament\Forms\Components\TextInput;
@@ -77,31 +76,35 @@ class UserResource extends Resource
     }
     public static function form(Form $form): Form
     {
+
         return $form
             ->schema([
                 Card::make()
                     ->schema([
-                        TextInput::make('name')
+                        'name'                 => TextInput::make('name')
                             ->label(strval(__('filament-authentication::filament-authentication.field.user.name')))
                             ->required(),
-                        TextInput::make('email')
+                        'email'                => TextInput::make('email')
                             ->required()
                             ->email()
                             ->unique(table: static::$model, ignorable: fn ($record) => $record)
                             ->label(strval(__('filament-authentication::filament-authentication.field.user.email'))),
-                        TextInput::make('password')
+                        'password'             => TextInput::make('password')
                             ->same('passwordConfirmation')
+                            ->hiddenOn('view')
+                            ->live(debounce: 250)
                             ->password()
                             ->maxLength(255)
                             ->required(fn ($component, $get, $livewire, $model, $record, $set, $state) => $record === null)
                             ->dehydrateStateUsing(fn ($state) => ! empty($state) ? Hash::make($state) : '')
                             ->label(strval(__('filament-authentication::filament-authentication.field.user.password'))),
-                        TextInput::make('passwordConfirmation')
+                        'passwordConfirmation' => TextInput::make('passwordConfirmation')
                             ->password()
                             ->dehydrated(false)
+                            ->visible(fn(Get $get) => filled($get('password')))
                             ->maxLength(255)
                             ->label(strval(__('filament-authentication::filament-authentication.field.user.confirm_password'))),
-                        Select::make('roles')
+                        'roles'                => Select::make('roles')
                             ->multiple()
                             ->relationship('roles', 'name')
                             ->preload(FilamentAuthentication::getPlugin()->getPreloadRoles())
@@ -110,78 +113,98 @@ class UserResource extends Resource
             ]);
     }
 
-    public static function table(Table $table): Table
+
+
+    protected static function getTableColumns(): array
     {
+        $columns = [
+            'id'                => TextColumn::make('id')
+                ->sortable()
+                ->label(strval(__('filament-authentication::filament-authentication.field.id'))),
+            'name'              => TextColumn::make('name')
+                ->searchable()
+                ->sortable()
+                ->label(strval(__('filament-authentication::filament-authentication.field.user.name'))),
+            'email'             => TextColumn::make('email')
+                ->searchable()
+                ->sortable()
+                ->label(strval(__('filament-authentication::filament-authentication.field.user.email'))),
 
-        $table = $table
-            ->columns([
-                TextColumn::make('id')
-                    ->sortable()
-                    ->label(strval(__('filament-authentication::filament-authentication.field.id'))),
-                TextColumn::make('name')
-                    ->searchable()
-                    ->sortable()
-                    ->label(strval(__('filament-authentication::filament-authentication.field.user.name'))),
-                TextColumn::make('email')
-                    ->searchable()
-                    ->sortable()
-                    ->label(strval(__('filament-authentication::filament-authentication.field.user.email'))),
-
-                IconColumn::make('email_verified_at')
-                ->default(false)
-                    ->boolean()
-                    ->label(strval(__('filament-authentication::filament-authentication.field.user.verified_at'))),
-                TextColumn::make('roles.name')->badge()
-                    ->label(strval(__('filament-authentication::filament-authentication.field.user.roles'))),
-                TextColumn::make('created_at')
-                    ->dateTime('Y-m-d H:i:s')
-                    ->label(strval(__('filament-authentication::filament-authentication.field.user.created_at'))),
-            ])
-            ->filters([
-                TernaryFilter::make('email_verified_at')
-                    ->label(strval(__('filament-authentication::filament-authentication.filter.verified')))
-                    ->nullable(),
-            ]);
-
-        $actions = [
-            ViewAction::make(),
-            EditAction::make(),
-            FilamentAuthentication::getPlugin()->impersonateEnabled() ? ImpersonateLink::make() : null,
-            DeleteAction::make(),
-            FilamentAuthentication::getPlugin()->usesSoftDeletes() ? ForceDeleteAction::make() : null,
-            FilamentAuthentication::getPlugin()->usesSoftDeletes() ? RestoreAction::make() : null,
+            'email_verified_at' => IconColumn::make('email_verified_at')
+            ->default(false)
+                ->boolean()
+                ->label(strval(__('filament-authentication::filament-authentication.field.user.verified_at'))),
+            'roles.name'        => TextColumn::make('roles.name')->badge()
+                ->label(strval(__('filament-authentication::filament-authentication.field.user.roles'))),
+            'created_at'        => TextColumn::make('created_at')
+                ->dateTime('Y-m-d H:i:s')
+                ->label(strval(__('filament-authentication::filament-authentication.field.user.created_at'))),
         ];
 
-        $bulkActions = [
+        if (in_array(LogsAuthentication::class, class_uses_recursive(FilamentAuthentication::getPlugin()->getModel('User')))) {
+            $columns['last_login'] = TextColumn::make('latestSuccessfullAuthentication.login_at')
+            ->dateTime('Y-m-d H:i:s')
+            ->description(fn(Model $record) => $record->latestSuccessfullAuthentication?->ip_address ?? '-')
+            ->label(strval(__('filament-authentication::filament-authentication.field.user.last_login_at')));
+        }
+
+        if (in_array(CanRenewPassword::class, class_uses_recursive(FilamentAuthentication::getPlugin()->getModel('User')))) {
+            $columns['last_password_changed'] = TextColumn::make('latestRenewable.created_at')
+            ->dateTime('Y-m-d H:i:s')
+                       ->label(strval(__('filament-authentication::filament-authentication.field.user.last_password_updated')));
+        }
+
+        return $columns;
+    }
+
+    protected static function getTableFilters(): array
+    {
+        $filters =  [
+            'email_verified_at' => TernaryFilter::make('email_verified_at')
+                ->label(strval(__('filament-authentication::filament-authentication.filter.verified')))
+                ->nullable(),
+        ];
+
+        if (FilamentAuthentication::getPlugin()->usesSoftDeletes()) {
+            $filters['trashed'] = \Filament\Tables\Filters\TrashedFilter::make();
+        }
+
+        return $filters;
+    }
+
+    protected static function getTableActions(): array
+    {
+        $actions = [
+            'view'         => ViewAction::make(),
+            'edit'         => EditAction::make(),
+            'impersonate'  => FilamentAuthentication::getPlugin()->impersonateEnabled() ? ImpersonateLink::make() : null,
+            'delete'       => DeleteAction::make(),
+            'force_delete' => FilamentAuthentication::getPlugin()->usesSoftDeletes() ? ForceDeleteAction::make() : null,
+            'restore'      => FilamentAuthentication::getPlugin()->usesSoftDeletes() ? RestoreAction::make() : null,
+        ];
+
+        return array_filter($actions);
+    }
+
+    protected static function getTableBulkActions(): array
+    {
+        $actions = [
             DeleteBulkAction::make(),
             FilamentAuthentication::getPlugin()->usesSoftDeletes() ? RestoreBulkAction::make() : null,
             FilamentAuthentication::getPlugin()->usesSoftDeletes() ? ForceDeleteBulkAction::make() : null,
         ];
 
-        $table->actions(Arr::whereNotNull($actions));
-        $table->bulkActions(Arr::whereNotNull($bulkActions));
+        return array_filter($actions);
+    }
 
-        if (FilamentAuthentication::getPlugin()->usesSoftDeletes()) {
-            $table->pushFilters([
-                \Filament\Tables\Filters\TrashedFilter::make()
-            ]);
-        }
 
-        if (in_array(LogsAuthentication::class, class_uses_recursive(FilamentAuthentication::getPlugin()->getModel('User')))) {
-            $table->pushColumns([TextColumn::make('latestSuccessfullAuthentication.login_at')
-            ->dateTime('Y-m-d H:i:s')
-            ->description(fn(Model $record) => $record->latestSuccessfullAuthentication?->ip_address ?? '-')
-            ->label(strval(__('filament-authentication::filament-authentication.field.user.last_login_at')))
-            ]);
-        }
-        if (in_array(CanRenewPassword::class, class_uses_recursive(FilamentAuthentication::getPlugin()->getModel('User')))) {
-            $table->pushColumns([TextColumn::make('latestRenewable.created_at')
-            ->dateTime('Y-m-d H:i:s')
-                       ->label(strval(__('filament-authentication::filament-authentication.field.user.last_password_updated')))
-            ]);
-        }
-
-        return $table;
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns(static::getTableColumns())
+            ->filters(static::getTableFilters())
+            ->actions(static::getTableActions())
+            ->bulkActions(static::getTableBulkActions());
     }
 
     public static function getRelations(): array
