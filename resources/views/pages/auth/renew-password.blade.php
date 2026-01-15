df8c62de21c78b01109dbe05229da644f0904796
Craig Smith <vxdhost@gmail.com>
2026-01-16 09:56:13 +1300

    Remove non-existent loginAction from renew password view

--- resources/views/pages/auth/renew-password.blade.php
+++ resources/views/pages/auth/renew-password.blade.php
@@ -1,12 +1,4 @@
 <x-filament-panels::page.simple>
-    @if (filament()->hasLogin())
-        <x-slot name="subheading">
-            {{ __('filament-panels::pages/auth/password-reset/request-password-reset.actions.login.label') }}
-
-            {{ $this->loginAction }}
-        </x-slot>
-    @endif
-
     <form wire:submit="renew">
         {{ $this->form }}
 
