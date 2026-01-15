64917cea34cd891d57adb06c08e76161e51558a7
Craig Smith <vxdhost@gmail.com>
2026-01-16 09:35:15 +1300

    Fix Filament 4 view compatibility - replace form.actions with button component

--- resources/views/pages/auth/renew-password.blade.php
+++ resources/views/pages/auth/renew-password.blade.php
@@ -1,11 +1,21 @@
 <x-filament-panels::page.simple>
+    @if (filament()->hasLogin())
+        <x-slot name="subheading">
+            {{ __('filament-panels::pages/auth/password-reset/request-password-reset.actions.login.label') }}
+
+            {{ $this->loginAction }}
+        </x-slot>
+    @endif
+
     <x-filament-panels::form wire:submit="renew">
         {{ $this->form }}
 
-        <x-filament-panels::form.actions
-                :actions="$this->getFormActions()"
-                :full-width="$this->hasFullWidthFormActions()"
-        />
+        <x-filament::button
+            type="submit"
+            form="renew"
+            class="w-full"
+        >
+            {{ __('filament-authentication::filament-authentication.pages.renew-password.actions.renew.label') }}
+        </x-filament::button>
     </x-filament-panels::form>
-
 </x-filament-panels::page.simple>
